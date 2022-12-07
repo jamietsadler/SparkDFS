be901edbd230eb3628a45d9b68f20cdb1d7a69e5
Jamie Sadler <jamietsadler@hotmail.co.uk>
2022-12-07 11:08:22 +0000

    Integrating Cassandra

--- src/main/scala/part4integrations/IntegratingCassandra.scala
+++ src/main/scala/part4integrations/IntegratingCassandra.scala
@@ -1,6 +1,10 @@
 package part4integrations
 
-import org.apache.spark.sql.SparkSession
+import com.datastax.spark.connector.cql.CassandraConnector
+import org.apache.spark.sql.{Dataset, ForeachWriter, SaveMode, SparkSession}
+import common._
+import org.apache.spark.sql.cassandra._
+import org.apache.spark.sql.functions.col
 
 object IntegratingCassandra {
 
@@ -9,13 +13,67 @@ object IntegratingCassandra {
     .master("local[2]")
     .getOrCreate()
 
+  import spark.implicits._
+
   def writeStreamToCassandraInBatches() = {
     val carsDS = spark.readStream
+      .schema(carsSchema)
+      .json("src/main/resources/data/cars")
+      .as[Car]
+
+    carsDS
+      .writeStream
+      .foreachBatch { (batch: Dataset[Car], batchId: Long) =>
+        // save batch to cassandra in single table write
+        batch.select(col("Name"), col("Horsepower"))
+          .write
+          .cassandraFormat("cars", "public")
+          .mode(SaveMode.Append)
+          .save()
+      }
+      .start()
+      .awaitTermination()
+  }
+
+  class carCassandraForeachWriter extends ForeachWriter[Car] {
+
+    val keyspace = "public"
+    val table = "cars"
+    val connector = CassandraConnector(spark.sparkContext.getConf)
+
+    override def open(partitionId: Long, epochId: Long): Boolean = {
+      println("Open Connection")
+      true
+    }
+    override def process(car: Car): Unit = {
+      connector.withSessionDo { session =>
+      session.execute(
+        s"""
+           |insert into $keyspace.$table("Name", "Horsepower")
+           |values ('${car.Name}', ${car.Horsepower}')
+           |""".stripMargin
+      )
+      }
+    }
+    override def close(errorOrNull: Throwable): Unit = println("closing connection")
 
   }
 
-  def main(args: Array[String]): Unit = {
+  def writeStreamToCassandra() = {
+    val carsDS = spark.readStream
+      .schema(carsSchema)
+      .json("src/main/resources/data/cars")
+      .as[Car]
 
+    carsDS
+      .writeStream
+      .foreach(new carCassandraForeachWriter)
+      .start()
+      .awaitTermination()
+  }
+
+  def main(args: Array[String]): Unit = {
+    writeStreamToCassandra()
   }
 
 }
